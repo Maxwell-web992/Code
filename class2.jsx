d9a8b9c9545b20a86e5ed1f147527749d6b61f71
Maxwell-web992 <maxwell.anglin@gmail.com>
2026-01-25 18:21:21 +0300

    Add files via upload

---
+++ class2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+class Student extends React.Component {
+  render() {
+    <h2>Hi am Maxwell</h2>
+  }
+}
+
+ReactDOM.render(<Student />, document.getElementById("root"));
