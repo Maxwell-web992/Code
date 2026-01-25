f7a4caf35cc7327fa5548f5d44288af13836eb10
Maxwell-web992 <maxwell.anglin@gmail.com>
2026-01-25 10:20:05 +0300

    Add files via upload

---
+++ React.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+const mycode = (
+
+    <table>
+        customer table
+        customer city
+        Ammount of purchase
+
+
+
+        Praveen
+        Maduri
+        12000
+
+
+        Kumar
+        Chennai
+        10000
+        );
+        );
+
+        
+
+        ReactDom.render(mycode, document.getElementById('root'));
+    </table>
+)
