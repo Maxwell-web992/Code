480af0eea20546ee47e59ddc7a7e602bf9237f95
Maxwell-web992 <maxwell.anglin@gmail.com>
2026-02-07 17:51:15 +0300

    Add files via upload

---
+++ After class external css.js
@@ -0,0 +1,36 @@
+class MyStyling extends React.Component {
+  render() {
+    const myheaderstyle = {
+      color: "blue",
+      backgroundColor: "SeaGreen",
+      fontfamily: "Arial",
+      padding: "5px",
+
+    };
+
+    const mystyle = {
+      color: "red",
+      backgroundColor: "LemonChiffon",
+      fontfamily: "Times New Roman  ",
+      padding: "10px",
+    };
+
+    const lifestyle = {
+      color: "blue",
+      backgroundColor: "Aquamarine",
+      fontfamily: "Times New Roman  ",
+      padding: "10px",
+    };
+
+    
+
+    return (
+      <div>
+        <h1 style={myheaderstyle}>Hi!</h1>
+        <p style={mystyle}>How are you?</p>
+      </div>
+    );
+  }
+}
+
+export default MyStyling;
