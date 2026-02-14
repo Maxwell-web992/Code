9e235cc8659962566b86a7fbed4886ff89acaf52
Maxwell-web992 <maxwell.anglin@gmail.com>
2026-02-14 17:36:35 +0300

    Add files via upload

---
+++ Lesson2.py
@@ -0,0 +1,12 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+
+#Print a number
+print(4)
+
+#Next line after print
+print("Hello python\n")
+
+#Print all the keywords in python
+import keyword
+print(keyword.kwlist)
