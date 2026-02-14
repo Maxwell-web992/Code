c923a9b49da22cfd27978b5669f159134a788c56
Maxwell-web992 <maxwell.anglin@gmail.com>
2026-02-14 17:28:31 +0300

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,12 @@
+# Printing a greeting message
+print("Hello,world!")
+
+# Printing my name
+print("My name is Maxwell.")
+
+#Printing a fact about myself
+#print ("I love playing soccer. ")
+print ("I love playing soccer. ")
+
+# Preforming and rinting a simple calculation
+print ("The result of 5 + 3 is", 5 + 3)
