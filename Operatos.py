eeb5920befb4bca9bcd375bb0ed785341adf7f75
Maxwell-web992 <maxwell.anglin@gmail.com>
2026-02-15 17:46:20 +0300

    Add files via upload

---
+++ Operatos.py
@@ -0,0 +1,19 @@
+num1=45
+num2=3
+
+print("Number1:",num1)
+print("Number2:",num2)
+print("Adition:",num1+num2)
+print("Difference:",num1-num2)
+print("Multiplication:",num1*num2)
+print("Division:",num1/num2)
+print("Floor Division:",num1//num2)
+print("Modulus:",num1%num2)
+print("square:",num1**2)
+print("square root:",num1**5)
+print("Equal ?",num1==num2)
+print("Number1 greater ?",num1>num2)
+print("Number2 greater ?",num1<num2)
+
+result = num1/2 + num2**2 + 10
+print(" Result of given operationis:",result)
