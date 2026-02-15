8dc6d1ca94e1da8edcb50f7428a293017e0df92b
Maxwell-web992 <maxwell.anglin@gmail.com>
2026-02-15 17:30:18 +0300

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,21 @@
+name="Polarbear"
+age=17
+is_student=True
+weight=45.5
+
+print("Name:", name)
+print("Data type of name:", type(name))
+print("Age:",age)
+print("Data type of age:", type(age))
+print("is_student:",is_student)
+print("Data type of is_student:", type(is_student))
+print("weight:",weight)
+print("data type of weight:",type (weight))  
+
+print("\n After Type Casting ....")
+age=str(age)
+print(age)
+print("Data type of age:", type(age))
+weight=int(weight)
+print(weight)
+print("Data type of weight:", type(weight))
