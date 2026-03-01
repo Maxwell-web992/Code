ef31dedd1a48e97930dabdff0f4bde536024bd6f
Maxwell-web992 <maxwell.anglin@gmail.com>
2026-03-01 17:50:44 +0300

    Add files via upload

---
+++ Triangle.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.Screen().bgcolor("LightGreen")
+board = turtle.Turtle()
+
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+
+turtle.done()
