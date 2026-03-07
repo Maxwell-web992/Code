1daf21b74aabe6f47b1ac603ea44a076c72fe5c7
Maxwell-web992 <maxwell.anglin@gmail.com>
2026-03-07 17:42:41 +0300

    Add files via upload

---
+++ Dictonary.py
@@ -0,0 +1,10 @@
+my_dict = {}
+
+my_dict = {1:'apple', 2: 'Ball'}
+
+my_dict = {'name': 'John', 1: [2, 3, 4]}
+
+my_dict = {'name': 'Jack', 'age':26}
+
+print(my_dict['name'])
+print(my_dict['age'])
